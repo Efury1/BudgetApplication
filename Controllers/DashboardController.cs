8f342223319a990d1cb01a4b646437d7a4734314
Eliza Fury <39455919+Efury1@users.noreply.github.com>
2025-03-14 16:48:37 +0000

    Add starting point for project

---
+++ Controllers/DashboardController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+public class DashboardController : Controller
+{
+    public IActionResult Dashboard()
+    {
+        // You can pass the data for the current user's dashboard, like budgets or money goals
+        return View();
+    }
+    
+}
---
+++ Models/BudgetModel.cs
@@ -0,0 +1,19 @@
+﻿public class BudgetModel
+{
+    public string? Name { get; set; }
+    public decimal Amount { get; set; }
+}
+
+public class MoneyGoalModel
+{
+    public string? Name { get; set; }
+    public decimal Amount { get; set; }
+}
+
+public class DashboardViewModel
+{
+    public List<BudgetModel>? Budgets { get; set; }
+    public List<MoneyGoalModel>? Goals { get; set; }
+    public decimal TotalSpent { get; set; }
+    public decimal RemainingBudget { get; set; }
+}
